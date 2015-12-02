c7a4ef46af7524de37a6117929f7e939f6a6cd6d
Felipe Gouveia <fmg@ecomp.poli.br>
2015-12-02 00:34:59 -0300

    Adicionando Accord e Criando classe para ler a mnist.

---
+++ c-sharp/mnist-hmm/mnist-hmm/DigitImage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mnist_hmm
+{
+    public class DigitImage
+    {
+        public byte[][] pixels;
+        public byte label;
+
+        public DigitImage(byte[][] pixels,
+          byte label)
+        {
+            this.pixels = new byte[28][];
+            for (int i = 0; i < this.pixels.Length; ++i)
+                this.pixels[i] = new byte[28];
+
+            for (int i = 0; i < 28; ++i)
+                for (int j = 0; j < 28; ++j)
+                    this.pixels[i][j] = pixels[i][j];
+
+            this.label = label;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < 28; ++i)
+            {
+                for (int j = 0; j < 28; ++j)
+                {
+                    if (this.pixels[i][j] == 0)
+                        s += " "; // white
+                    else if (this.pixels[i][j] == 255)
+                        s += "O"; // black
+                    else
+                        s += "."; // gray
+                }
+                s += "\n";
+            }
+            s += this.label.ToString();
+            return s;
+        } // ToString
+
+    }
+}
