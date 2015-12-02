a67b494ea840364d2e40ea7f63a93b9f33104c3c
Felipe Gouveia <fmg@ecomp.poli.br>
2015-12-02 00:44:21 -0300

    Adicionando Accord.Statistics

--- c-sharp/mnist-hmm/mnist-hmm/MnistReader.cs
+++ c-sharp/mnist-hmm/mnist-hmm/MnistReader.cs
@@ -7,64 +7,53 @@ using System.Threading.Tasks;
 
 namespace mnist_hmm
 {
+    /// <summary>
+    /// From https://jamesmccaffrey.wordpress.com/2013/11/23/reading-the-mnist-data-set-with-c/
+    /// </summary>
     public class MnistReader
     {
         public static List<DigitImage> Read(string labelsFile, string imagesFile)
         {
             List<DigitImage> images = new List<DigitImage>();
-            try
+            FileStream ifsLabels = new FileStream(labelsFile, FileMode.Open); // test labels
+            FileStream ifsImages = new FileStream(imagesFile, FileMode.Open); // test images
+
+            BinaryReader brLabels = new BinaryReader(ifsLabels);
+            BinaryReader brImages = new BinaryReader(ifsImages);
+
+            int magic1 = brImages.ReadInt32(); // discard
+            int numImages = brImages.ReadInt32();
+            int numRows = brImages.ReadInt32();
+            int numCols = brImages.ReadInt32();
+
+            int magic2 = brLabels.ReadInt32();
+            int numLabels = brLabels.ReadInt32();
+
+            byte[][] pixels = new byte[28][];
+            for (int i = 0; i < pixels.Length; ++i)
+                pixels[i] = new byte[28];
+
+            // each test image
+            for (int di = 0; di < 10000; ++di)
             {
-                Console.WriteLine("\nBegin\n");
-                FileStream ifsLabels = new FileStream(@"C:\t10k-labels.idx1-ubyte", FileMode.Open); // test labels
-                FileStream ifsImages = new FileStream(@"C:\t10k-images.idx3-ubyte", FileMode.Open); // test images
-
-                BinaryReader brLabels = new BinaryReader(ifsLabels);
-                BinaryReader brImages = new BinaryReader(ifsImages);
-
-                int magic1 = brImages.ReadInt32(); // discard
-                int numImages = brImages.ReadInt32();
-                int numRows = brImages.ReadInt32();
-                int numCols = brImages.ReadInt32();
-
-                int magic2 = brLabels.ReadInt32();
-                int numLabels = brLabels.ReadInt32();
-
-                byte[][] pixels = new byte[28][];
-                for (int i = 0; i < pixels.Length; ++i)
-                    pixels[i] = new byte[28];
-
-                // each test image
-                for (int di = 0; di < 10000; ++di)
+                for (int i = 0; i < 28; ++i)
                 {
-                    for (int i = 0; i < 28; ++i)
+                    for (int j = 0; j < 28; ++j)
                     {
-                        for (int j = 0; j < 28; ++j)
-                        {
-                            byte b = brImages.ReadByte();
-                            pixels[i][j] = b;
-                        }
+                        byte b = brImages.ReadByte();
+                        pixels[i][j] = b;
                     }
+                }
 
-                    byte lbl = brLabels.ReadByte();
+                byte lbl = brLabels.ReadByte();
 
-                    DigitImage dImage = new DigitImage(pixels, lbl);
-                    Console.WriteLine(dImage.ToString());
-                    Console.ReadLine();
-                } // each image
+                DigitImage dImage = new DigitImage(pixels, lbl);
+            } // each image
 
-                ifsImages.Close();
-                brImages.Close();
-                ifsLabels.Close();
-                brLabels.Close();
-
-                Console.WriteLine("\nEnd\n");
-                Console.ReadLine();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.ReadLine();
-            }
+            ifsImages.Close();
+            brImages.Close();
+            ifsLabels.Close();
+            brLabels.Close();
             return images;
         }
     }
---
+++ c-sharp/mnist-hmm/mnist-hmm/MnistTrainer.cs
@@ -0,0 +1,28 @@
+﻿using Accord.Statistics.Models.Markov;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mnist_hmm
+{
+    public class MnistTrainer
+    {
+        private List<DigitImage> trainData;
+        private List<DigitImage> testData;
+
+        private List<HiddenMarkovModel> hmms;
+
+        public void ReadData()
+        {
+            trainData = MnistReader.Read(Properties.Settings.Default.TrainLabels, Properties.Settings.Default.TrainImages);
+            testData = MnistReader.Read(Properties.Settings.Default.TestLabels, Properties.Settings.Default.TestImages);
+        }
+
+        public void Train()
+        {
+            HiddenMarkovModel hmm = new HiddenMarkovModel(3, 2);
+        }
+    }
+}
