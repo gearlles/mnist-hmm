486a4fee6fa394242446ece22bfd7ac4488389a7
Felipe Gouveia <fmg@ecomp.poli.br>
2015-12-02 00:26:13 -0300

    Criando projeto em c#

---
+++ c-sharp/mnist-hmm/mnist-hmm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mnist_hmm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
